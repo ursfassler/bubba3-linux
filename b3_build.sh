b01f1a88e3ab71a289d838981ebc71a3727e6a03
Urs Fässler <urs@bitzgi.ch>
2016-11-23 15:38:44 +0100

    b3 build script

---
+++ b3_build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+set -e
+
+LoadAddr=00008000
+
+prepare ()
+{
+  # not sure about this
+  cp arch/arm/configs/bubba3_defconfig .config
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- olddefconfig
+}
+
+build_image ()
+{
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j `nproc` LOADADDR=${LoadAddr} zImage kirkwood-b3.dtb
+}
+
+build_modules ()
+{
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j `nproc` LOADADDR=${LoadAddr} modules
+}
+
+package_image()
+{
+  cat header.raw > zImage
+  cat arch/arm/boot/zImage >> zImage
+  cat arch/arm/boot/dts/kirkwood-b3.dtb >> zImage
+  mkimage -A arm -O linux -T kernel -C none -a ${LoadAddr} -e ${LoadAddr} -n Linux-B3 -d zImage uImage
+  rm zImage
+}
+
+package_modules()
+{
+  modulesDir=./modules
+  rm -rf ${modulesDir}
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=${modulesDir} modules_install
+  cd ${modulesDir}
+  tar -cvzf ../modules.tar.gz *
+}
+
+
+
+prepare
+
+build_image
+package_image
+
+build_modules
+package_modules
+
+echo "all done"
+
